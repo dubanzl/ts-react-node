dff904cb5822535b5f5119b842c836dc280306a2
andreszl <andreszl.devcode@gmail.com>
2019-12-27 21:03:45 -0500

    frontend auth

--- client/src/components/auth/register.component.tsx
+++ client/src/components/auth/register.component.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Form, Button, Icon } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
-import '../../stylesheet/auth/login.less';
+import '../../stylesheet/auth/register.less';
 
 interface Props {
 	history: any;
@@ -10,7 +10,7 @@ interface Props {
 interface State {
 }
 
-class Login extends Component<Props, State> {
+class Register extends Component<Props, State> {
 	constructor(props) {
 		super(props);
 		this.state = {};
@@ -20,9 +20,9 @@ class Login extends Component<Props, State> {
 		const { history } = this.props;
 
 		return (
-			<div className="login">
-				<p className="login-brand">Task Manager <Icon inverted color="blue" name="tasks" circular /></p>
-				<div className="login-box">
+			<div className="register">
+				<p className="register-brand">Task Manager <Icon inverted color="blue" name="tasks" circular /></p>
+				<div className="register-box">
 					<Form size="large">
 						<Form.Input
 							fluid
@@ -43,13 +43,20 @@ class Login extends Component<Props, State> {
 							<Icon className="display-password" name="eye" />
 						</Form.Input>
 
+						<Form.Input
+							icon
+							type="password"
+							placeholder="Confirmar Contraseña"
+						>
+							<input />
+							<Icon className="password-lock-icon" name="lock" />
+							<Icon className="display-password" name="eye" />
+						</Form.Input>
+
 						<Button color="blue" fluid size="large">
-							INCIAR SESIÓN
-						</Button>
-						<label className="link" onClick={() => { history.push('/registro'); }}> ¿No tienes cuenta? registrate aquí </label>
-						<Button color="black" fluid size="large" onClick={() => { history.push('/registro'); }}>
 							REGISTRARSE
 						</Button>
+						<label className="link" onClick={() => { history.push('/iniciar-sesion'); }}> ¿Ya tienes cuenta? inicia sesión aquí</label>
 					</Form>
 				</div>
 			</div>
@@ -58,4 +65,4 @@ class Login extends Component<Props, State> {
 }
 
 
-export default withRouter((Login) as any);
+export default withRouter((Register) as any);
--- client/src/routes/auth.routes.ts
+++ client/src/routes/auth.routes.ts
@@ -1,9 +1,15 @@
 import Login from '../components/auth/login.component';
+import Register from '../components/auth/register.component';
 
 export const authRoutes : any = [
 	{
 		path: '/iniciar-sesion',
 		exact: true,
 		component: Login,
+	},
+	{
+		path: '/registro',
+		exact: true,
+		component: Register,
 	},
 ];
